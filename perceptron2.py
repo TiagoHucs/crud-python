9cc772fe6b2d15f8fcf85db04e9b50fbcf84c465
Tiago S. Hucs <tiagohucs@gmail.com>
2024-07-03 15:54:47 +0000

    aplicacao simples e aplicacao com numpy

--- perceptron2.py
+++ perceptron2.py
@@ -1,11 +1,11 @@
-entradas = [-1 , 7 , 5]
-pesos = [0.8 , 0.1 , 0]
+import numpy as np
+
+
+entradas = np.array([-1 , 7 , 5])
+pesos = np.array([0.8 , 0.1 , 0])
 
 def soma(e , p):
-    s = 0
-    for i in range(3):
-        s += e[i] * p[i]
-    return s
+    return e.dot(p)
 
 s = soma(entradas, pesos)
 print('soma: ', s)
