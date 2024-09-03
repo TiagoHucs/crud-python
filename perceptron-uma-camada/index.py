c980a6782afeef399cb8564b1280e54a18d1dcf2
Tiago Santos Hucs <tiago.hucs@spread.com.br>
2024-09-03 13:26:56 -0300

    recomeçando

--- perceptron-uma-camada/index.py
+++ perceptron-uma-camada/index.py
@@ -1,4 +1,4 @@
-entradas = [-1 , 7 , 5]
+entradas = [1 , 7 , 5]
 pesos = [0.8 , 0.1 , 0]
 
 def soma(e , p):
@@ -7,12 +7,12 @@ def soma(e , p):
         s += e[i] * p[i]
     return s
 
-s = soma(entradas, pesos)
-print('soma: ', s)
 def stepFunction(soma):
     if (soma >= 1):
         return 1
     return 0
 
+s = soma(entradas, pesos)
 r = stepFunction(s)
+
 print('stepFunction: ', r)
